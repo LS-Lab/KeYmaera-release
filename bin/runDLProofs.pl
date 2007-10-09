833004c35ba8cd14dee610da49bab1f0f2f549cb
Jan-David Quesel <quesel@informatik.uni-oldenburg.de>
2007-10-09 15:39:37 +0200

    printing timeout in ms

--- bin/runDLProofs.pl
+++ bin/runDLProofs.pl
@@ -250,7 +250,8 @@ sub runAuto {
 	die "unexpected error" unless $@ eq "alarm\n";
 	alarm 0;
     open (STS, ">>$statfile");
-	print STS "$realfilename, NA, $timeout, $headerfile, $expectedresult, TIMEOUT\n";
+	my $printtimeout = $timeout*1000;
+	print STS "$realfilename, NA, $printtimeout, $headerfile, $expectedresult, TIMEOUT\n";
 	close(STS);
 	return 2;
   } else {
