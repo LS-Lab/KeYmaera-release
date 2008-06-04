4c599f85b44ae509463459de5adb5c0c54516032
Timo Michelsen <timo.michelsen@uni-oldenburg.de>
2008-06-04 13:48:13 +0200

    added comments in QepCadInput.java first try to use JUnit in QebCadInputTest.java in test-package

--- system/de/uka/ilkd/key/dl/arithmetics/impl/qepcad/QepCadInput.java
+++ system/de/uka/ilkd/key/dl/arithmetics/impl/qepcad/QepCadInput.java
@@ -7,14 +7,25 @@ package de.uka.ilkd.key.dl.arithmetics.impl.qepcad;
  */
 public class QepCadInput {
 	
-	private String description = "";
+	private String description = "[]";
 	private String variableList = "";
 	private int freeVariableNum = 0;
 	private String formula = "";
 	
+	/**
+	 * Standardconstructor
+	 */
 	public QepCadInput() {		
 	}
 	
+	/**
+	 * Constructor. Applies the given Parameters.
+	 * 
+	 * @param desc Description of the formula
+	 * @param varList List of variables in the formula
+	 * @param freeVarNum Number of free variables
+	 * @param formula Formula
+	 */
 	public QepCadInput( String desc, String varList, int freeVarNum, String formula ) {
 		setDescription( desc );
 		setVariableList( varList );
@@ -22,26 +33,55 @@ public class QepCadInput {
 		setFormula( formula );
 	}
 	
+	/**
+	 * Retrieves the description
+	 * @return Description
+	 */
 	public String getDescription() {
 		return this.description;
 	}
 	
+	/**
+	 * Sets the description
+	 * @param desc New description
+	 */
 	public void setDescription( String desc ) {
 		this.description = desc;
 	}
 	
+	/**
+	 * Retrieves the variablelist as string
+	 * 
+	 * @return Variablelist
+	 */
 	public String getVariableList() {
 		return this.variableList;
 	}
 	
+	/**
+	 * Sets the list of Variables. Format like "(x,y)"
+	 * 
+	 * @param varList New list of variables
+	 */
 	public void setVariableList( String varList ) {
 		this.variableList = varList;
 	}
 	
+	/**
+	 * Gets the number of free variables
+	 * 
+	 * @return Number of free variables
+	 */
 	public int getFreeVariableNum() {
 		return this.freeVariableNum;
 	}
 	
+	/**
+	 * Sets the number of variables. Negative
+	 * numbers are not allowed.
+	 * 
+	 * @param val New number of variables
+	 */
 	public void setFreeVariableNum( int val ) {
 		if( val < 0) 
 			val = 0;
@@ -49,10 +89,20 @@ public class QepCadInput {
 		this.freeVariableNum = val;
 	}
 	
+	/**
+	 * Gets the formula
+	 * 
+	 * @return Formula
+	 */
 	public String getFormula() {
 		return this.formula;
 	}
 	
+	/**
+	 * Sets the formula
+	 * 
+	 * @param formula New formula
+	 */
 	public void setFormula( String formula ) {
 		this.formula = formula;
 	}
