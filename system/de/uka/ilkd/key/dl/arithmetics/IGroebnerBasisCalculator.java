d16a8c61ba89d85f0505b2c53030681430aff3e9
Jan-David Quesel <qusel@informatik.uni-oldenburg.de>
2008-06-06 18:08:19 +0200

    added classes

---
+++ system/de/uka/ilkd/key/dl/arithmetics/IGroebnerBasisCalculator.java
@@ -0,0 +1,39 @@
+/***************************************************************************
+ *   Copyright (C) 2008 by Jan-David Quesel                                *
+ *   quesel@informatik.uni-oldenburg.de                                    *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+package de.uka.ilkd.key.dl.arithmetics;
+
+import java.rmi.RemoteException;
+
+import de.uka.ilkd.key.dl.arithmetics.impl.SumOfSquaresChecker.PolynomialClassification;
+import de.uka.ilkd.key.logic.Term;
+
+/**
+ * @author jdq
+ * TODO Documentation since Jun 6, 2008
+ */
+public interface IGroebnerBasisCalculator {
+
+	/**
+	 * @param terms
+	 * TODO documentation since Jun 6, 2008
+	 * @throws RemoteException 
+	 */
+	boolean checkForConstantGroebnerBasis(PolynomialClassification<Term> terms) throws RemoteException;
+}
