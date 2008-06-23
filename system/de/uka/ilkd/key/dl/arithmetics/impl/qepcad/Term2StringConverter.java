b845716d3b0996ca10925011e5dc07bb1ccebad1
Timo Michelsen <timo.michelsen@uni-oldenburg.de>
2008-06-23 14:20:10 +0200

    continued: Term2StringConverter.java 80% implemented (Quantifier remains...) added first successfull test for Term2StringConverter

--- system/de/uka/ilkd/key/dl/arithmetics/impl/qepcad/Term2StringConverter.java
+++ system/de/uka/ilkd/key/dl/arithmetics/impl/qepcad/Term2StringConverter.java
@@ -61,111 +61,131 @@ public class Term2StringConverter {
             } else if (f.name().toString().equals("mul")) {
             	return "(" + args[0] + "*" + args[1] + ")";
             } else if (f.name().toString().equals("div")) {
-                for (Expr e : args) {
-                    boolean rational = e.numberQ();
-                    if (!rational) {
-                        return new Expr(DIV, args);
-                    }
-                }
-                return new Expr(RATIONAL, args);
+            	return "(" + args[0] + "/" + args[1] + ")";
+            	// TODO: check
+//                for (Expr e : args) {
+//                    boolean rational = e.numberQ();
+//                    if (!rational) {
+//                        return new Expr(DIV, args);
+//                    }
+//                }
+//                return new Expr(RATIONAL, args);
             } else if (f.name().toString().equals("exp")) {
-                return new Expr(EXP, args);
+            	return "(" + args[0] + "^" + args[1] + ")";
             } else {
                 try {
                     BigDecimal d = new BigDecimal(form.op().name().toString());
                     try {
-                        return new Expr(d.intValueExact());
+                    	return String.valueOf(d.intValueExact());
                     } catch (ArithmeticException e) {
-                        return new Expr(Expr.SYM_REAL,
-                                new Expr[] { new Expr(d) });
+                    	return "<TODO>"; // TODO : Change this
+//                        return new Expr(Expr.SYM_REAL,
+//                                new Expr[] { new Expr(d) });
                     }
                 } catch (NumberFormatException e) {
                     String name = form.op().name().toString();
-                    name = name.replaceAll("_", USCORE_ESCAPE);
-                    Expr expr = new Expr(Expr.SYMBOL, name);
+//                    name = name.replaceAll("_", USCORE_ESCAPE);
+//                    Expr expr = new Expr(Expr.SYMBOL, name);
                     if (args.length == 0) {
-                        return expr;
+                        return "(" + name + ")";
                     }
-                    return new Expr(expr, args);
+                    return "(" + name + "(" + array2String(args) + "))";
+// 					  return new Expr(expr, args);
                 }
             }
         } else if (form.op() instanceof LogicVariable
                 || form.op() instanceof de.uka.ilkd.key.logic.op.ProgramVariable
                 || form.op() instanceof Metavariable) {
-            String name = form.op().name().toString();
-            name = name.replaceAll("_", USCORE_ESCAPE);
-            return new Expr(Expr.SYMBOL, name);
+        	return form.op().name().toString();
+//            String name = form.op().name().toString();
+//            name = name.replaceAll("_", USCORE_ESCAPE);
+//            return new Expr(Expr.SYMBOL, name);
         } else if (form.op() instanceof Junctor) {
             if (form.op() == Junctor.AND) {
-                return new Expr(AND, args);
+            	return "(" + args[0] + "/\\" + args[1] + ")";
             } else if (form.op() == Junctor.OR) {
-                return new Expr(OR, args);
+            	return "(" + args[0] + "\\/" + args[1] + ")";
             } else if (form.op() == Junctor.IMP) {
-                return new Expr(IMPL, args);
+            	return "(" + args[0] + "==>" + args[1] + ")";
             } else if (form.op() == Junctor.NOT) {
-                return new Expr(NOT, args);
+                return "(~" + args[0] + ")";
             }
-        } else if (form.op() instanceof Quantifier) {
-            Expr[] newArgs = new Expr[args.length + 1];
-            System.arraycopy(args, 0, newArgs, 1, args.length);
-            Expr[] vars = new Expr[form.varsBoundHere(0).size()];
-            for (int i = 0; i < vars.length; i++) {
-                vars[i] = new Expr(Expr.SYMBOL, form.varsBoundHere(0)
-                        .getQuantifiableVariable(i).name().toString()
-                        .replaceAll("_", USCORE_ESCAPE));
-            }
-            newArgs[0] = new Expr(LIST, vars);
-            if (form.op() == Quantifier.ALL) {
-                assert args.length == 1 : "'Unary' KeY quantifier \\forall x ("
-                        + args + ")";
-                if (args[0].head() == FORALL) {
-                    assert args[0].args().length == 2 : "'Binary' quantifier with variables and formula";
-                    Expr kernel = args[0].args()[1];
-                    assert args[0].args()[0].head() == LIST : "Term2ExprConverter always builds list quantifiers";
-                    Expr[] innerVariables = args[0].args()[0].args();
-                    // allVariables = (outer)vars + innerVariables
-                    Expr[] allVariables = new Expr[innerVariables.length
-                            + vars.length];
-                    System.arraycopy(vars, 0, allVariables, 0, vars.length);
-                    System.arraycopy(innerVariables, 0, allVariables,
-                            vars.length, innerVariables.length);
-                    Expr[] mergedQuant = new Expr[args.length + 1];
-                    assert mergedQuant.length == 2;
-                    mergedQuant[0] = new Expr(LIST, allVariables);
-                    mergedQuant[1] = kernel;
-                    Expr expr = new Expr(FORALL, mergedQuant);
-                    assert equalsTranslateBack(QuantifierType.FORALL, expr, new Expr(FORALL, newArgs));
-                    return expr;
-                } else {
-                    return new Expr(FORALL, newArgs);
-                }
-            } else if (form.op() == Quantifier.EX) {
-                assert args.length == 1 : "'Unary' KeY quantifier \\exists x ("
-                        + args + ")";
-                if (args[0].head() == EXISTS) {
-                    assert args[0].args().length == 2 : "'Binary' quantifier with variables and formula";
-                    Expr kernel = args[0].args()[1];
-                    assert args[0].args()[0].head() == LIST : "Term2ExprConverter always builds list quantifiers";
-                    Expr[] innerVariables = args[0].args()[0].args();
-                    // allVariables = (outer)vars + innerVariables
-                    Expr[] allVariables = new Expr[innerVariables.length
-                            + vars.length];
-                    System.arraycopy(vars, 0, allVariables, 0, vars.length);
-                    System.arraycopy(innerVariables, 0, allVariables,
-                            vars.length, innerVariables.length);
-                    Expr[] mergedQuant = new Expr[args.length + 1];
-                    assert mergedQuant.length == 2;
-                    mergedQuant[0] = new Expr(LIST, allVariables);
-                    mergedQuant[1] = kernel;
-                    Expr expr = new Expr(EXISTS, mergedQuant);
-                    assert equalsTranslateBack(QuantifierType.EXISTS, expr, new Expr(EXISTS, newArgs)) : "backtranslation identity";
-                    return expr;
-                } else {
-                    return new Expr(EXISTS, newArgs);
-                }
-            }
-        }
+        } 
+//        else if (form.op() instanceof Quantifier) {
+//            Expr[] newArgs = new Expr[args.length + 1];
+//            System.arraycopy(args, 0, newArgs, 1, args.length);
+//            Expr[] vars = new Expr[form.varsBoundHere(0).size()];
+//            for (int i = 0; i < vars.length; i++) {
+//                vars[i] = new Expr(Expr.SYMBOL, form.varsBoundHere(0)
+//                        .getQuantifiableVariable(i).name().toString()
+//                        .replaceAll("_", USCORE_ESCAPE));
+//            }
+//            newArgs[0] = new Expr(LIST, vars);
+//            if (form.op() == Quantifier.ALL) {
+//                assert args.length == 1 : "'Unary' KeY quantifier \\forall x ("
+//                        + args + ")";
+//                if (args[0].head() == FORALL) {
+//                    assert args[0].args().length == 2 : "'Binary' quantifier with variables and formula";
+//                    Expr kernel = args[0].args()[1];
+//                    assert args[0].args()[0].head() == LIST : "Term2ExprConverter always builds list quantifiers";
+//                    Expr[] innerVariables = args[0].args()[0].args();
+//                    // allVariables = (outer)vars + innerVariables
+//                    Expr[] allVariables = new Expr[innerVariables.length
+//                            + vars.length];
+//                    System.arraycopy(vars, 0, allVariables, 0, vars.length);
+//                    System.arraycopy(innerVariables, 0, allVariables,
+//                            vars.length, innerVariables.length);
+//                    Expr[] mergedQuant = new Expr[args.length + 1];
+//                    assert mergedQuant.length == 2;
+//                    mergedQuant[0] = new Expr(LIST, allVariables);
+//                    mergedQuant[1] = kernel;
+//                    Expr expr = new Expr(FORALL, mergedQuant);
+//                    assert equalsTranslateBack(QuantifierType.FORALL, expr, new Expr(FORALL, newArgs));
+//                    return expr;
+//                } else {
+//                    return new Expr(FORALL, newArgs);
+//                }
+//            } else if (form.op() == Quantifier.EX) {
+//                assert args.length == 1 : "'Unary' KeY quantifier \\exists x ("
+//                        + args + ")";
+//                if (args[0].head() == EXISTS) {
+//                    assert args[0].args().length == 2 : "'Binary' quantifier with variables and formula";
+//                    Expr kernel = args[0].args()[1];
+//                    assert args[0].args()[0].head() == LIST : "Term2ExprConverter always builds list quantifiers";
+//                    Expr[] innerVariables = args[0].args()[0].args();
+//                    // allVariables = (outer)vars + innerVariables
+//                    Expr[] allVariables = new Expr[innerVariables.length
+//                            + vars.length];
+//                    System.arraycopy(vars, 0, allVariables, 0, vars.length);
+//                    System.arraycopy(innerVariables, 0, allVariables,
+//                            vars.length, innerVariables.length);
+//                    Expr[] mergedQuant = new Expr[args.length + 1];
+//                    assert mergedQuant.length == 2;
+//                    mergedQuant[0] = new Expr(LIST, allVariables);
+//                    mergedQuant[1] = kernel;
+//                    Expr expr = new Expr(EXISTS, mergedQuant);
+//                    assert equalsTranslateBack(QuantifierType.EXISTS, expr, new Expr(EXISTS, newArgs)) : "backtranslation identity";
+//                    return expr;
+//                } else {
+//                    return new Expr(EXISTS, newArgs);
+//                }
+//            }
+//        }
         throw new IllegalArgumentException("Could not convert Term: " + form
                 + "Operator was: " + form.op());
+	}
+	
+	public static String array2String( String[] args ) {
+		if( args == null )
+			return "";
+		
+		String result = "";
+		for( int i = 0; i < args.length; i++ ) {
+			result += args[i];
+			if(i != args.length - 1) 
+				result += ",";
+		}
+		
+		return result;
 	}
 }
--- system/de/uka/ilkd/key/dl/arithmetics/impl/qepcad/test/Term2StringConverterTest.java
+++ system/de/uka/ilkd/key/dl/arithmetics/impl/qepcad/test/Term2StringConverterTest.java
@@ -1,6 +1,5 @@
 package de.uka.ilkd.key.dl.arithmetics.impl.qepcad.test;
 
-
 import de.uka.ilkd.key.dl.arithmetics.impl.qepcad.Term2StringConverter;
 import de.uka.ilkd.key.logic.Name;
 import de.uka.ilkd.key.logic.Term;
@@ -13,8 +12,8 @@ import junit.framework.TestCase;
 public class Term2StringConverterTest extends TestCase {
 
 	public void test_convert() {
-		
-		// Creates the term '(not a) or b' 
+
+		// Creates the term '(not a) or b'
 		TermBuilder tb = TermBuilder.DF;
 
 		RigidFunction rigidFunction = new RigidFunction(new Name("A"),
@@ -22,10 +21,29 @@ public class Term2StringConverterTest extends TestCase {
 		RigidFunction rigidFunction2 = new RigidFunction(new Name("B"),
 				Sort.FORMULA, new Sort[0]);
 
-		Term term = tb.or(tb.not(tb.func(rigidFunction)), tb.func(rigidFunction2));
-		
+		Term term = tb.or(tb.not(tb.func(rigidFunction)), tb
+				.func(rigidFunction2));
+
 		String termString = Term2StringConverter.convert2String(term);
-		
-		assertEquals("~a /\\ b", termString);
+
+		assertEquals("((~(A))\\/(B))", termString);
+	}
+
+	public void test_convert2() {
+
+		// Creates the term '(2 * a) + c = b'
+		TermBuilder tb = TermBuilder.DF;
+
+		RigidFunction a = new RigidFunction(new Name("A"), Sort.FORMULA,
+				new Sort[0]);
+		RigidFunction b = new RigidFunction(new Name("B"), Sort.FORMULA,
+				new Sort[0]);
+		RigidFunction c = new RigidFunction(new Name("B"), Sort.FORMULA,
+				new Sort[0]);		
+	}
+
+	public void test_arrayConvert() {
+		String[] args = new String[] { "a", "b", "c", "d" };
+		assertEquals("a,b,c,d", Term2StringConverter.array2String(args));
 	}
 }
