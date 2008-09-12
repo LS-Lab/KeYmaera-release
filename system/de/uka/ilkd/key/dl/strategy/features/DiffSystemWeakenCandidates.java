1ebb467fef6fde981b27352372542fd34ee26a63
Jan-David Quesel <qusel@informatik.uni-oldenburg.de>
2008-09-12 15:34:13 +0200

    added classes for last commit to compile

---
+++ system/de/uka/ilkd/key/dl/strategy/features/DiffSystemWeakenCandidates.java
@@ -0,0 +1,181 @@
+/***************************************************************************
+ *   Copyright (C) 2008 by Jan David Quesel                                *
+ *   quesel@informatik.uni-oldenburg.de                                    *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+package de.uka.ilkd.key.dl.strategy.features;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+
+import orbital.util.Setops;
+
+import de.uka.ilkd.key.dl.model.DLProgram;
+import de.uka.ilkd.key.dl.model.DiffSystem;
+import de.uka.ilkd.key.dl.model.Equals;
+import de.uka.ilkd.key.dl.model.Expression;
+import de.uka.ilkd.key.dl.model.Formula;
+import de.uka.ilkd.key.dl.model.PredicateTerm;
+import de.uka.ilkd.key.dl.model.TermFactory;
+import de.uka.ilkd.key.dl.model.impl.TermFactoryImpl;
+import de.uka.ilkd.key.dl.strategy.termProjection.Generator;
+import de.uka.ilkd.key.java.ProgramElement;
+import de.uka.ilkd.key.java.StatementBlock;
+import de.uka.ilkd.key.logic.JavaBlock;
+import de.uka.ilkd.key.logic.PosInOccurrence;
+import de.uka.ilkd.key.logic.Term;
+import de.uka.ilkd.key.logic.op.Modality;
+import de.uka.ilkd.key.logic.op.QuanUpdateOperator;
+import de.uka.ilkd.key.proof.Goal;
+import de.uka.ilkd.key.rule.RuleApp;
+
+/**
+ * @author jdq
+ * @since Sep 9, 2008
+ */
+public class DiffSystemWeakenCandidates implements Generator<ProgramElement> {
+
+	public static final Generator<ProgramElement> INSTANCE = new DiffSystemWeakenCandidates();
+
+	public Iterator<ProgramElement> generate(RuleApp app, PosInOccurrence pos, Goal goal) {
+
+		Term term = pos.subTerm();
+		// unbox from update prefix
+		while (term.op() instanceof QuanUpdateOperator) {
+			term = ((QuanUpdateOperator) term.op()).target(term);
+		}
+		if (!(term.op() instanceof Modality && term.javaBlock() != null
+				&& term.javaBlock() != JavaBlock.EMPTY_JAVABLOCK && term
+				.javaBlock().program() instanceof StatementBlock)) {
+			throw new IllegalArgumentException("inapplicable to " + pos);
+		}
+		final DLProgram program = (DLProgram) ((StatementBlock) term
+				.javaBlock().program()).getChildAt(0);
+
+		TermFactory tf;
+		try {
+			tf = TermFactory.getTermFactory(TermFactoryImpl.class, goal.proof()
+					.getNamespaces());
+
+			DiffSystem one = (DiffSystem) program;
+			List<Formula> forms = new LinkedList<Formula>();
+			for (ProgramElement f : one) {
+				if (one.isDifferentialEquation(f)) {
+					if (f instanceof PredicateTerm) {
+						PredicateTerm p = (PredicateTerm) f;
+						if (p.getChildAt(0) instanceof Equals) {
+							forms.add((Formula) f);
+						} else {
+							List<Expression> children = new LinkedList<Expression>();
+							children.add((Expression) p.getChildAt(1));
+							children.add((Expression) p.getChildAt(2));
+							forms.add(tf.createPredicateTerm(tf.createEquals(),
+									children));
+						}
+					} else {
+						throw new IllegalArgumentException(
+								"Rule not applicable to complex terms");
+					}
+				} else {
+					forms.add((Formula) f);
+				}
+			}
+
+			Queue<ProgramElement> candidates = new LinkedList<ProgramElement>();
+
+			// create cross product of formulas
+			Set<Formula> curForms = new LinkedHashSet<Formula>(forms);
+			Set powerset = Setops.powerset(curForms);
+
+			for (Object s: powerset) {
+				Set set = (Set) s;
+				if(!set.isEmpty()) {
+					List<Formula> can = new LinkedList<Formula>();
+					for(Object f : set) {
+						Formula form = (Formula) f;
+						can.add(form);
+					}
+					candidates.add(tf.createDiffSystem(can));
+				}
+			}
+			return new PEIterator(candidates);
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return new PEIterator(new LinkedList<ProgramElement>());
+	}
+
+	public static class PEIterator implements Iterator<ProgramElement> {
+
+		private Queue<ProgramElement> terms;
+
+		/**
+		 * 
+		 */
+		public PEIterator(Queue<ProgramElement> terms) {
+			this.terms = terms;
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see de.uka.ilkd.key.logic.IteratorOfTerm#hasNext()
+		 */
+		@Override
+		public boolean hasNext() {
+			return !terms.isEmpty();
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see de.uka.ilkd.key.logic.IteratorOfTerm#next()
+		 */
+		@Override
+		public ProgramElement next() {
+			return terms.poll();
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see java.util.Iterator#remove()
+		 */
+		@Override
+		public void remove() {
+			terms.poll();
+		}
+
+	}
+
+}
---
+++ system/de/uka/ilkd/key/dl/strategy/features/ProgramSVInstantiationCP.java
@@ -0,0 +1,119 @@
+// This file is part of KeY - Integrated Deductive Software Design
+// Copyright (C) 2001-2005 Universitaet Karlsruhe, Germany
+//                         Universitaet Koblenz-Landau, Germany
+//                         Chalmers University of Technology, Sweden
+//
+// The KeY system is protected by the GNU General Public License. 
+// See LICENSE.TXT for details.
+//
+//
+
+
+package de.uka.ilkd.key.dl.strategy.features;
+
+import de.uka.ilkd.key.dl.strategy.termProjection.Buffer;
+import de.uka.ilkd.key.java.ProgramElement;
+import de.uka.ilkd.key.logic.Name;
+import de.uka.ilkd.key.logic.PosInOccurrence;
+import de.uka.ilkd.key.logic.op.IteratorOfSchemaVariable;
+import de.uka.ilkd.key.logic.op.SchemaVariable;
+import de.uka.ilkd.key.logic.op.SetOfSchemaVariable;
+import de.uka.ilkd.key.proof.Goal;
+import de.uka.ilkd.key.rule.RuleApp;
+import de.uka.ilkd.key.rule.TacletApp;
+import de.uka.ilkd.key.strategy.LongRuleAppCost;
+import de.uka.ilkd.key.strategy.RuleAppCost;
+import de.uka.ilkd.key.strategy.feature.Feature;
+import de.uka.ilkd.key.strategy.feature.instantiator.BackTrackingManager;
+import de.uka.ilkd.key.strategy.feature.instantiator.CPBranch;
+import de.uka.ilkd.key.strategy.feature.instantiator.ChoicePoint;
+import de.uka.ilkd.key.util.Debug;
+
+
+/**
+ * Feature representing a <code>ChoicePoint</code> for instantiating a schema
+ * variable of a taclet with the term that is returned by a
+ * <code>ProjectionToTerm</code>. This feature is useful in particular
+ * combined with <code>ForEachCP</code>. Although the feature formally is a
+ * choice point, it will always have exactly one branch
+ */
+public class ProgramSVInstantiationCP implements Feature {
+
+    private final BackTrackingManager manager;
+
+    private final Name svToInstantiate;
+    private final Buffer<ProgramElement> value;
+
+    public static Feature create(Name svToInstantiate,
+    		Buffer<ProgramElement> value,
+                                 BackTrackingManager manager) {
+        return new ProgramSVInstantiationCP ( svToInstantiate, value, manager );
+    }
+        
+    private ProgramSVInstantiationCP(Name svToInstantiate,
+    		Buffer<ProgramElement> value,
+                              BackTrackingManager manager) {
+        this.svToInstantiate = svToInstantiate;
+        this.value = value;
+        this.manager = manager;
+    }
+
+    public RuleAppCost compute(RuleApp app, PosInOccurrence pos, Goal goal) {
+        manager.passChoicePoint ( new CP (app, pos, goal), this );
+        return LongRuleAppCost.ZERO_COST;
+    }
+
+    private SchemaVariable findSVWithName(TacletApp app) {
+        final SetOfSchemaVariable vars = app.uninstantiatedVars ();
+        final IteratorOfSchemaVariable it = vars.iterator ();
+        while ( it.hasNext () ) {
+            final SchemaVariable svt = it.next ();
+            if ( svt.name ().equals ( svToInstantiate ) ) return svt;
+        }
+        
+        Debug.fail ( "Did not find schema variable "
+                     + svToInstantiate + " that I was supposed to instantiate\n" +
+                     "(taclet " + app.taclet().name() + ")\n" +
+                     "Either the name of the variable is wrong, or the variable\n" +
+                     "has already been instantiated as " + app.instantiations().lookupValue(svToInstantiate)+ "." );
+        return null;
+    }
+
+    
+    private class CP implements ChoicePoint {
+        
+        private final PosInOccurrence pos;
+        private final RuleApp         app;
+        private final Goal            goal;
+    
+        private CP(RuleApp app, PosInOccurrence pos, Goal goal) {
+            this.pos = pos;
+            this.app = app;
+            this.goal = goal;
+        }
+
+        public de.uka.ilkd.key.strategy.feature.instantiator.IteratorOfCPBranch getBranches(RuleApp oldApp) {
+            if ( ! ( oldApp instanceof TacletApp ) )
+                Debug.fail ( "Instantiation feature is only applicable to " +
+                             "taclet apps, but got " + oldApp );
+            final TacletApp tapp = (TacletApp)oldApp;
+            
+            final SchemaVariable sv = findSVWithName ( tapp );
+            final ProgramElement pe = value.to( app, pos, goal );
+
+            final RuleApp newApp =
+                tapp.addCheckedInstantiation ( sv,
+                                               pe,
+                                               goal.proof ().getServices (),
+                                               true );
+
+            final CPBranch branch = new CPBranch () {
+                public void choose() {}
+                public RuleApp getRuleAppForBranch() { return newApp; }
+            };
+            
+            return de.uka.ilkd.key.strategy.feature.instantiator.SLListOfCPBranch.EMPTY_LIST.prepend ( branch ).iterator ();
+        }
+        
+    }
+}
