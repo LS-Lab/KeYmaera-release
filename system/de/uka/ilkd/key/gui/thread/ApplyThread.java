d1e01bde03dcfcfaaa696d6ffed8a98ed3e90889
Timo Michelsen <timo.michelsen@uni-oldenburg.de>
2009-05-25 13:50:34 +0200

    Implemented ApplyThread - no tested yet

--- system/de/uka/ilkd/key/gui/thread/ApplyThread.java
+++ system/de/uka/ilkd/key/gui/thread/ApplyThread.java
@@ -3,16 +3,32 @@ package de.uka.ilkd.key.gui.thread;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import de.uka.ilkd.key.proof.Goal;
+import de.uka.ilkd.key.proof.ListOfGoal;
+import de.uka.ilkd.key.rule.RuleApp;
+
 public class ApplyThread implements Runnable, IThreadSender {
 
     private Collection<IThreadListener> listeners = new ArrayList<IThreadListener>();
+    private final Goal goal;
+    private final RuleApp app;
+    private ListOfGoal goalList;
+    
+    public ApplyThread( RuleApp app, Goal goal ) {
+        this.goal = goal;
+        this.app = app;
+    }
+    
+    public ListOfGoal getListOfGoal() {
+        return goalList;
+    }
     
     @Override
     public void run() {
         notifyStart();
-        
-        // Hier apply aufrufen
-        
+        if( goal != null && app != null ) {
+            goalList = goal.apply(app);
+        }
         notifyEnd();
     }
 
