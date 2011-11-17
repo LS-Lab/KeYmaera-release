c7c863b69276db1525d57ff96ab00090c1950141
Jan-David Quesel <quesel@informatik.uni-oldenburg.de>
2011-11-17 11:07:11 +0100

    unhide the "applyGammaRules" option

--- system/de/uka/ilkd/key/dl/options/DLOptionBeanBeanInfo.java
+++ system/de/uka/ilkd/key/dl/options/DLOptionBeanBeanInfo.java
@@ -143,7 +143,7 @@ public class DLOptionBeanBeanInfo extends SimpleBeanInfo {
 							"readdQuantifiers", EPropertyConstant.DLOPTIONS_READD_QUANTIFIERS,true, false, false),
 					createDescriptor(
 							"applyGammaRules", EPropertyConstant.DLOPTIONS_APPLY_GAMMA_RULES,
-							true, false, true, ApplyRulesPropertyEditor.class),
+							true, false, false, ApplyRulesPropertyEditor.class),
 					createDescriptor(
 							"applyUpdatesToModalities", EPropertyConstant.DLOPTIONS_APPLY_UPDATES_TO_MODALITIES,
 							true, false),
