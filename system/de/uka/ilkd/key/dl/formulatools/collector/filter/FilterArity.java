5e7c0df4fcc13079a2ed49ab899c23621253fcfe
Jan-David Quesel <quesel@informatik.uni-oldenburg.de>
2012-03-19 12:19:19 +0100

    change licence header

--- system/de/uka/ilkd/key/dl/formulatools/collector/filter/FilterArity.java
+++ system/de/uka/ilkd/key/dl/formulatools/collector/filter/FilterArity.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2012 Jan-David Quesel.
+ * Copyright (c) 2009 Timo Michelsen.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Public License v2.0
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * 
  * Contributors:
- *     Jan-David Quesel - initial API and implementation
+ *     Timo Michelsen - initial API and implementation
  ******************************************************************************/
 package de.uka.ilkd.key.dl.formulatools.collector.filter;
 
