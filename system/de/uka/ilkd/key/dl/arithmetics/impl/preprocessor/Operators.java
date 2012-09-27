542d57661bbf34e67e1d134143ef89445f40a58b
Andrew Sogokon <s0805753@sms.ed.ac.uk>
2012-09-27 12:26:43 +0100

    New binary tree model

--- system/de/uka/ilkd/key/dl/arithmetics/impl/preprocessor/Operators.java
+++ system/de/uka/ilkd/key/dl/arithmetics/impl/preprocessor/Operators.java
@@ -1,94 +1,112 @@
+/************************************************************************
+ *  KeYmaera-MetiTarski interface.
+ *  Copyright (C) 2012  s0805753@sms.ed.ac.uk University of Edinburgh.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ *  
+ ************************************************************************/
+
 package de.uka.ilkd.key.dl.arithmetics.impl.preprocessor;
 
+/**
+ * Enum type for translating KeYmaera operators to back-end solver syntax.
+ * <p>
+ * N.B. Here we set the Enum constant names to match those of KeYmaera
+ * operators, this allows us to perform translation in constant time.
+ * </p>
+ * <p>
+ * N.B. The special function operators for KeY are in fact those returned by
+ * <i>Mathematica™</i>.</p>
+ * 
+ * @author s0805753@sms.ed.ac.uk
+ * @since 12/02/2012
+ */
 public enum Operators {
-   	
-	/**
-	 * Enum type for translating KeYmaera operators to back-end solver syntax.
-	 * <p>
-	 * <b>N.B.</b> Here we set the Enum constant names to match those of KeYmaera 
-	 * operators, this allows us to perform translation in constant time.  
-	 * 
-	 * @author s0805753@sms.ed.ac.uk
-	 * @since 12/02/2012
-	 */
 
-	and	("and", " ∧ ", "/\\", "&"),
-	or	("or", " ∨ ", "\\/", "|"),
-	not 	("not", " ¬ ", "not" , "~"),
-	imp	("imp", " → ", "imp", "=>"),
-	equiv	("equiv", "↔", "equiv", "equiv"),
-	all 	("all", "∀ ", "all", "!"),
-	exist 	("exist", "∃ ", "exist", "?"),
-	
-	mul	("mul", "·", "*", "*"),
-	neg	("neg", "-", "-", "-"),
-	div	("div", "÷", "/", "/"),
-	add	("add", "+", "+", "+"),
-	sub	("sub", "-", "-", "-"),
-	exp	("exp", "^", "^", "^"),
-		
-	equals	("equals", "=", "=", "="),	
-	geq	("geq", "≥", ">=", ">="),
-	neq	("neq", "≠", "!=", "!="),
-	leq	("leq", "≤", "<=", "<="),
-	lt	("lt", "<", "<", "<"),
-	gt	("gt", ">", ">", ">"),
-		
-	Log	("Log", "ln"),
-	Exp	("Exp", "exp"),
+   not      (  "not"    ,  " ¬ "    ,  "not"    ,  "~"      )  ,
+   and      (  "and"    ,  " ∧ "    ,  "/\\"    ,  "&"      )  ,
+   or       (  "or"     ,  " ∨ "    ,  "\\/"    ,  "|"      )  ,
+   imp      (  "imp"    ,  " → "    ,  "imp"    ,  "=>"     )  ,
+   equiv    (  "equiv"  ,  " ↔ "    ,  "equiv"  ,  "equiv"  )  ,
+   all      (  "all"    ,  "∀ "     ,  "all"    ,  "!"      )  ,
+   exist    (  "exist"  ,  "∃ "     ,  "exist"  ,  "?"      )  ,
 
-	Sin	("Sin", "sin"),
-	Cos	("Cos", "cos"),
-	Tan	("Tan", "tan"),
-	
-	Sinh	("Sinh", "sinh"),
-	Cosh	("Cosh", "cosh"),
-	Tanh	("Tanh", "tanh"),
+   neg      (  "neg"    ,  "-"      ,  "-"      ,  "-"      )  ,
+   exp      (  "exp"    ,  "^"      ,  "^"      ,  "^"      )  ,
+   mul      (  "mul"    ,  "·"      ,  "*"      ,  "*"      )  ,
+   div      (  "div"    ,  "÷"      ,  "/"      ,  "/"      )  ,
+   add      (  "add"    ,  "+"      ,  "+"      ,  "+"      )  ,
+   sub      (  "sub"    ,  "-"      ,  "-"      ,  "-"      )  ,
+      
+   equals   (  "equals" ,  "="      ,  "="      ,  "="      )  ,  
+   geq      (  "geq"    ,  "≥"      ,  ">="     ,  ">="     )  ,
+   neq      (  "neq"    ,  "≠"      ,  "!="     ,  "!="     )  ,
+   leq      (  "leq"    ,  "≤"      ,  "<="     ,  "<="     )  ,
+   lt       (  "lt"     ,  "<"      ,  "<"      ,  "<"      )  ,
+   gt       (  "gt"     ,  ">"      ,  ">"      ,  ">"      )  ,
+         
+   Log      (  "Log"    ,  "ln"                             )  ,
+   Exp      (  "Exp"    ,  "exp"                            )  ,
 
-	ArcSin	("ArcSin", "asin"),
-	ArcCos	("ArcCos", "acos"),
-	ArcTan	("ArcTan", "atan"),
-	
-	// Auxiliary
-	VERUM 	("true",  "⊤", "VERUM",  "$true"),
-	FALSUM 	("false", "⊥", "FALSUM", "$false"),
-	
-	// MetiTarski square & cube roots; ugly but necessary.
-	SQRT	("Sqrt", "√", "sqrt", "sqrt"),
-	CBRT	("Cbrt", "∛", "cbrt", "cbrt");
+   Sin      (  "Sin"    ,  "sin"                            )  ,
+   Cos      (  "Cos"    ,  "cos"                            )  ,
+   Tan      (  "Tan"    ,  "tan"                            )  ,
+   
+   Sinh     (  "Sinh"   ,  "sinh"                           )  ,
+   Cosh     (  "Cosh"   ,  "cosh"                           )  ,
+   Tanh     (  "Tanh"   ,  "tanh"                           )  ,
+
+   ArcSin   (  "ArcSin" ,  "asin"                           )  ,
+   ArcCos   (  "ArcCos" ,  "acos"                           )  ,
+   ArcTan   (  "ArcTan" ,  "atan"                           )  ,
+   
+   VERUM    (  "true"   ,  "⊤"      ,  "VERUM"  ,  "$true"  )  ,
+   FALSUM   (  "false"  ,  "⊥"      ,  "FALSUM" ,  "$false" )  ,
+   
+   SQRT     (  "Sqrt"   ,  "√"      ,  "sqrt"   ,  "sqrt"   )  ,
+   CBRT     (  "Cbrt"   ,  "∛"      ,  "cbrt"   ,  "cbrt"   )  ;
 
    public String KeY, utf, Rahd, Tptp;
 
-   Operators(String KeY, String utf, String Rahd, String Tptp){
-      this.KeY = KeY;
-      this.utf=utf;
-      this.Rahd=Rahd;
-      this.Tptp=Tptp;  		
+   Operators(String KeY, String utf, String Rahd, String Tptp) {
+      this. KeY   =  KeY;
+      this. utf   =  utf;
+      this. Rahd  =  Rahd;
+      this. Tptp  =  Tptp;       
    }
 
    Operators(String KeY, String Tptp) {
-      this.KeY = KeY;
-      this.utf = KeY;
-      this.Rahd = KeY;
-      this.Tptp = Tptp;
+      this. KeY   =  KeY;
+      this. utf   =  KeY;
+      this. Rahd  =  KeY;
+      this. Tptp  =  Tptp;
    } 
 
    public Operators negatePredicate(){
       return Operators.negatePredicate(this);
    }
 
-   public static Operators negatePredicate(Operators operator){
-      switch(operator){
-         case geq :	return lt;
-         case leq :	return gt;
-         case lt :	return geq;
-         case gt :	return leq;
-         case equals :	return neq;
-         case neq :	return equals;
-
-         default :	
-                     //logger.severe("\""+operator.name() + "\" is not a binary predicate, cannot negate!");
-                     return operator;
+   private static Operators negatePredicate(Operators operator) {
+      switch(operator)
+      {
+         case geq    :  return lt;
+         case leq    :  return gt;
+         case lt     :  return geq;
+         case gt     :  return leq;
+         case equals :  return neq;
+         case neq    :  return equals;
+         default     :  return operator;
       }
-   }    	    
+   }         
 }
