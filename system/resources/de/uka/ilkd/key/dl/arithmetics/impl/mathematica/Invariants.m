de26ab085e551d566519b27c36a19a09a9c649f0
Khalil Ghorbal <khalil.ghorbal@gmail.com>
2014-03-12 02:21:04 -0400

    updating DRI rules list + Invariants.m

--- system/resources/de/uka/ilkd/key/dl/arithmetics/impl/mathematica/Invariants.m
+++ system/resources/de/uka/ilkd/key/dl/arithmetics/impl/mathematica/Invariants.m
@@ -1,11 +1,13 @@
 
 (* Copyright (C) Andrew Sogokon and Khalil Ghorbal 2014 *)
-
-(* This package is released under GPL license. Please read the COPYRIGHT file *)  
+(* email. khalil.ghorbal@gmail.com *)
+(* This package is released under GPL license. Please read the COPYRIGHT file *)
 
 (* ::Package:: *)
 
 BeginPackage["Invariants`"]
+
+SOSToList::usage="SOSToList[sos_] Turn a sum-of-squares into a list (real equivalence, if interpreted as a conjunction)."
 
 PolynomialDegree::usage="PolynomialDegree[p_] Computes the degree of a polynomial argument."
 
@@ -29,11 +31,17 @@ DI::usage="DI[p_,f_,vars_List] Differential Induction for equational atoms."
 
 DRI::usage="DRI[h_,Lx_,vars_] Differential Radical Invariant. Lx: polynomial vector field, h: polynomial invariant candidate, vars: state variables"
 
-SFDRI::usage="DRI[h_,Lx_,vars_] Differential Radical Invariant on SF reduced candidate. Lx: polynomial vector field, h: polynomial invariant candidate, vars: state variables"
+DRIBuggy::usage="DRI[h_,Lx_,vars_] Differential Radical Invariant. Lx: polynomial vector field, h: polynomial invariant candidate, vars: state variables"
+
+SFDRI::usage="SFDRI[h_,Lx_,vars_] Differential Radical Invariant after SF reduction. Lx: polynomial vector field, h: polynomial invariant candidate, vars: state variables"
+
+SoSDRI::usage="SoSDRI[h_,Lx_,vars_] Differential Radical Invariant. Lx: polynomial vector field, h: a list of polynomials, vars: state variables"
+
+DRIConj::usage="DRIConj[h_List, Lx_, vars_] Conjunctive Differential Radical Invariant. Lx: polynomial vector field, h: polynomial invariant candidate variety, vars: state variables"
 
 Darboux::usage="Darboux[h_,Lx_,vars_] Darboux Polynomials (Rational Integrals). Lx: polynomial vector field, h: polynomial invariant candidate, vars: state variables"
 
-SFDarboux::usage="SFDarboux[h_,Lx_,vars_] Darboux Polynomials (Rational Integrals) on SF reduced candidate. Lx: polynomial vector field, h: polynomial invariant candidate, vars: state variables"
+SFDarboux::usage="Darboux[h_,Lx_,vars_] Darboux Polynomials after SF reduction. Lx: polynomial vector field, h: polynomial invariant candidate, vars: state variables"
 
 Lie::usage="Lie[p_,f_,vars_List] Lie's criterion for invariance of smooth differentiable manifolds."
 
@@ -48,12 +56,15 @@ SFLieStar::usage="SFLieStar[p_,f_,vars_List] Modified Lie's criterion applied on
 
 Begin["`Private`"]
 
+SOSToList[sos_]:=Module[{},Map[Function[x,x/.{Power[a_,2] -> a}],If[TrueQ[Head[sos]==Plus],Apply[List,sos],{sos}]]]
+
 PolynomialDegree[p_,vars_List]:=Module[{},
 Max[Map[Plus @@ # &, CoefficientRules[p, vars][[All, 1]]]]
 ]
 
-VectorFieldDegree[f_,vars_List]:=Module[{},
-Max[Map[PolynomialDegree[#, vars]&,f[[All, 2]]]]
+(* Expects a list of polynomials *)
+VectorFieldDegree[f_List,vars_List]:=Module[{},
+Max[Map[PolynomialDegree[#, vars]&,f]]
 ]
 
 UniversalClosure[F_,vars_List]:=Module[{},
@@ -90,8 +101,8 @@ LieDerivative[p,f,vars]==0,
 vars]
 ]
 
-(* uses SF reduction for the QE problem but not for the ideal membership *)
-DRI[h_, Lx_, vars_] := Catch[Module[{N = 1, SFh=SF[h], L, GB, LX = Lx /. {Rule[a_, b_] -> b}, Remainder, Reduction = True},
+(* Buggy CAV version *)
+DRIBuggy[h_, Lx_, vars_] := Catch[Module[{N = 1, SFh=SF[h], L, GB, LX = Lx /. {Rule[a_, b_] -> b}, Remainder, Reduction = True},
 L = {h};
 While[True,
   If[N<2,Remainder = PolynomialReduce[LieDerivative[h, Lx, vars], {h}, vars][[2]],
@@ -99,28 +110,61 @@ While[True,
   MonomialOrder -> DegreeReverseLexicographic];
   Remainder = PolynomialReduce[L[[-1]], GB, vars][[2]];
   ]
-  If[PossibleZeroQ[Remainder], Throw[True],
+  If[PossibleZeroQ[Remainder], Throw[{True,StringForm["(N=``)", N]}],
    AppendTo[L, LieDerivative[L[[N]], Lx, vars]];
    Reduction = Reduction && TrueQ[Reduce[UniversalClosure[Implies[SFh == 0, SF[L[[-1]]] == 0], vars], vars, Reals]];
-   If[Not[Reduction], Throw[False], N++]
+   If[Not[Reduction], Throw[{False,StringForm["(N=``)", N]}], N++]
   ]
  ]
 ]]
 
-(* uses SF reduction everywhere *)
-SFDRI[h_, Lx_, vars_] := Module[{SFh=SF[h]},
-DRI[SFh,Lx,vars]
+(* uses SF reduction for the QE problem but not for the ideal membership *)
+DRI[h_, Lx_, vars_] := Catch[Module[{lieD, N = 1, SFh=SF[h], L={h}, GB, Remainder, Reduction = True,allvars=Variables[Join[{h},Lx]]},
+While[True,
+  lieD=LieDerivative[L[[N]], Lx, vars];
+  If[N<2,Remainder = PolynomialReduce[lieD, {h}, vars][[2]],
+  GB = GroebnerBasis[L, vars, 
+  MonomialOrder -> DegreeReverseLexicographic];
+  Remainder = PolynomialReduce[lieD, GB, vars,MonomialOrder -> DegreeReverseLexicographic][[2]];
+  ]
+  If[PossibleZeroQ[Remainder], Throw[{True,StringForm["(N=``)", N]}],
+   AppendTo[L, lieD];
+   Reduction = Reduction && TrueQ[Reduce[UniversalClosure[Implies[SFh == 0, SF[L[[-1]]] == 0], allvars], allvars, Reals]];
+   If[Reduction, N++, Throw[{False,StringForm["(N=``)", N]}]]
+  ]
+ ]
+]]
+
+SoSDRI[V_List, Lx_, vars_]:=Module[{h=SumOfSquares[V]},
+If[Length[V]==1,DRI[V[[1]],Lx,vars],DRI[h, Lx, vars]]
 ]
 
-Darboux[h_, Lx_, vars_] := Catch[Module[{LX = Lx /. {Rule[a_, b_] -> b}, Remainder},
+(* uses SF reduction everywhere *)
+SFDRI[h_, Lx_, vars_] := Module[{},DRI[SF[h],Lx,vars]]
+
+DRIConj[h_List, Lx_, vars_] := Catch[Module[{N = 1,r = Length[h], conj, range, L=h, GB, RemainderZQ=False, Reduction = True,tmp={},GBL=h, allvars=Variables[Join[h,Lx]]},
+If[r==1,DRI[h[[1]],Lx,vars],
+range=Range[1,r];
+conj=And@@Flatten[Reap[Do[Sow[SF[L[[i]]] == 0], {i, 1, r}]][[2]]];
+While[True,
+  GB = GroebnerBasis[GBL, vars, MonomialOrder -> DegreeReverseLexicographic];
+  tmp =  Map[LieDerivative[L[[-#]], Lx, vars] &, range];
+  L = Join[L,tmp];
+  RemainderZQ = And@@Map[PossibleZeroQ[PolynomialReduce[L[[-#]], GB, vars,MonomialOrder -> DegreeReverseLexicographic][[2]]]&,range];
+  If[RemainderZQ, Throw[{True,StringForm["(N=``)", N]}],
+   GBL = Join[GB, tmp];
+   Reduction = Reduction && And@@Map[TrueQ[Reduce[UniversalClosure[Implies[conj, SF[L[[-#]]] == 0], allvars], allvars, Reals]]&,range];
+   If[Reduction, N++, Throw[{False,StringForm["(N=``)", N]}]];
+  ]
+ ]
+]]]
+
+Darboux[h_, Lx_, vars_] := Catch[Module[{Remainder},
   Remainder = PolynomialReduce[LieDerivative[h, Lx, vars], {h}, vars][[2]];
   If[PossibleZeroQ[Remainder], Throw[True], Throw[False]]
 ]]
 
-(* SF reduction + Darboux *)
-SFDarboux[h_, Lx_, vars_] := Module[{SFh=SF[h]},
-  Darboux[SFh,Lx,vars]
-]
+SFDarboux[h_, Lx_, vars_] := Module[{SFh=SF[h]},Darboux[SFh,Lx,vars]]
 
 Lie[p_,f_,vars_List]:=Module[{},
 UniversalClosure[
@@ -145,7 +189,7 @@ Implies[SFp==0,LieDerivative[SFp,f,vars]==0  && Implies[SingularLocus[SFp,vars],
 vars]
 ]
 
-SFLieStar[p_,f_,vars_List]:=Module[{SFp = SF[p]},
+SFLieStar[p_,f_,vars_List]:=Module[{SFp = SF[p],P,TAU},
 P=SFp/.{Map[Function[v,Rule[v,v+TAU*Derivative[1][v]]], vars]};
 UniversalClosure[
 Implies[SFp==0,LieDerivative[SFp,f,vars]==0  && Implies[SingularLocus[SFp,vars], (P/.f/.{Rule[a_,b_]-> Rule[F[a],b]})[[1]]==0]],
@@ -154,6 +198,21 @@ Join[vars,{TAU}]]
 
 End[ ]
 EndPackage[ ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
